b945766190a241d5546f5f4c94799c89e89c36b7
carefree_ladka <vermapawan087@gmail.com>
2024-12-30 21:19:28 +0530

    Updates

--- worker.js
+++ worker.js
@@ -1,13 +1,21 @@
 self.onmessage = (event) => {
   const code = event.data;
   try {
-    // Override console.log to send messages back to the main thread
-    console.log = (args) => {
-      self.postMessage(args);
+    // Override console.log to send all messages back to the main thread in one line
+    console.log = (...args) => {
+      let message = args.map(arg => {
+        if (typeof arg === 'object' && arg !== null) {
+          return JSON.stringify(arg);
+        } else {
+          return arg.toString();
+        }
+      }).join(" "); // Concatenate all arguments into a single string with spaces
+
+      self.postMessage(message);
     };
 
     // Execute the code
-     (new Function(code))();
+    (new Function(code))();
   } catch (error) {
     // If an error occurs, send the error message back to the main thread
     self.postMessage(error.toString());
